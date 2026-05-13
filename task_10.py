786b211f1996765197b99eb86e6b275144908d0f
Apteemka <zlobin.a2008@mail.ru>
2026-05-13 14:39:59 +0700

    Add files via upload

---
+++ task_10.py
@@ -0,0 +1,23 @@
+import time
+from cProfile import label
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@timer
+
+
+def heavy_computation():
+    return sum(i*i for i in range(n))
+
+
+numbers = [1, 2, 3, 4, 5]
+squared = list(map(lambda x: x * x, numbers))
+filtered = list(filter(lambda x: x > 10, numbers))
